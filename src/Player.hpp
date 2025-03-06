34730f65c4e03b6cf89245455617b2199e679f2b
Tanner Davison <tanner.davison95@gmail.com>
2025-03-05 22:47:18 -0700

    updated player speed stats for mac

--- src/Player.hpp
+++ src/Player.hpp
@@ -1,13 +1,15 @@
 #pragma once
 #include "SDL_rect.h"
 #include "weapon.hpp"
+
 #include <SDL_image.h>
+
 #include <utility>
 
 struct SDL_Renderer;
 
 class Player {
-private:
+ private:
   // smooth movement float points
   float rectXf;
   float rectYf;
@@ -24,22 +26,22 @@ private:
   float velocityX;
   float velocityY;
 #ifdef _WIN32
-  const float MAX_VELOCITY = 5.4f;       // Maximum velocity
-  const float BASE_ACCELERATION = .70f;  // BASE_ACCELERATION
-  const float BOOST_ACCELERATION = 3.5f; // BOOST_ACCELERATION
-  const float DECELERATION = .9830;      // DECELERATION
+  const float MAX_VELOCITY = 5.4f;        // Maximum velocity
+  const float BASE_ACCELERATION = .70f;   // BASE_ACCELERATION
+  const float BOOST_ACCELERATION = 3.5f;  // BOOST_ACCELERATION
+  const float DECELERATION = .9830;       // DECELERATION
   float ACCELERATION = .05f;
 #else
-  const float MAX_VELOCITY = 3.4f;       // Maximum velocity
-  const float BASE_ACCELERATION = .40f;  // BASE_ACCELERATION
-  const float BOOST_ACCELERATION = 3.5f; // BOOST_ACCELERATION
-  const float DECELERATION = .9330;      // DECELERATION
+  const float MAX_VELOCITY = 3.4f;        // Maximum velocity
+  const float BASE_ACCELERATION = .40f;   // BASE_ACCELERATION
+  const float BOOST_ACCELERATION = 3.5f;  // BOOST_ACCELERATION
+  const float DECELERATION = .9730;       // DECELERATION
   float ACCELERATION = .05f;
 
 #endif
   // Textures
   SDL_Rect playerRect;
-  SDL_Texture *mTexture;
+  SDL_Texture* mTexture;
   int textureWidth;
   int textureHeight;
   // Movement state
@@ -52,25 +54,29 @@ private:
   // Weapon system
   Weapon weapon;
 
-public:
+ public:
   Player();
   Player(int x, int y);
   Player(int x, int y, int width, int height);
   ~Player();
   // Core functions
-  void renderPlayer(SDL_Renderer *renderer);
+  void renderPlayer(SDL_Renderer* renderer);
   // input and pos
-  void handlePlayerInputAndPosition(const Uint8 *keyState);
+  void handlePlayerInputAndPosition(const Uint8* keyState);
   void handleBoundsAndUpdatePosition(float nextX, float nextY);
-  bool loadTexture(const char *path, SDL_Renderer *renderer);
-  void cleanup(); // Texture
-  bool checkCollision(const SDL_Rect &a, const SDL_Rect &b);
+  bool loadTexture(const char* path, SDL_Renderer* renderer);
+  void cleanup();  // Texture
+  bool checkCollision(const SDL_Rect& a, const SDL_Rect& b);
 
-  const Weapon &getWeapon() const;
-  Weapon &getWeapon();
+  const Weapon& getWeapon() const;
+  Weapon& getWeapon();
   std::pair<int, int> getPosition() const;
 
   // get sizes
-  int getWidth() const { return rectWidth; }
-  int getHeight() const { return rectHeight; }
+  int getWidth() const {
+    return rectWidth;
+  }
+  int getHeight() const {
+    return rectHeight;
+  }
 };
